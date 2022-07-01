2277df332d98ddce1b929df12dd6eb3611a4257d
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-07-01 15:11:50 +0600

    add DuplicateZeros.py

--- LeetCode/Python/DuplicateZeros.py
+++ LeetCode/Python/DuplicateZeros.py
@@ -4,7 +4,7 @@ from typing import List
 class DuplicateZeros:
 
     def __init__(self):
-        self.duplicateZeros([1,5,2,0,6,8,0,6,0])
+        self.duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])
 
     def duplicateZeros(self, arr: List[int]) -> None:
         """
@@ -13,18 +13,15 @@ class DuplicateZeros:
 
         i = 0
         while i < len(arr):
+            # print(f"i:: {i}")
             if arr[i] == 0:
-                if i + 1 < len(arr) - 1:
-                    for k in range(i + 2, len(arr)).__reversed__():
-                        arr[k] = arr[k - 1]
+                for k in range(i + 2, len(arr)).__reversed__():
+                    # print(f"k:: {k}")
+                    arr[k] = arr[k - 1]
+                if i + 1 < len(arr):
                     arr[i + 1] = 0
-                    print(arr)
+                print(arr)
                 i += 2
             else:
                 i += 1
         print(arr)
-
-    def duplicateZerosOptimized(self, arr: List[int]) -> None:
-        """
-        Do not return anything, modify arr in-place instead.
-        """
