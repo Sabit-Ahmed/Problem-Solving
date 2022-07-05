a7064b8ad3e99944c1f11ea333d37705c20141ef
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-07-05 17:41:53 +0600

    add accepted RemoveElement.py

--- LeetCode/Python/RemoveElement.py
+++ LeetCode/Python/RemoveElement.py
@@ -4,17 +4,34 @@ from typing import List
 class RemoveElement:
 
     def __init__(self):
-        nums = [3, 2, 2, 3]
+        nums = [3,2,2,3]
         val = 3
         res = self.removeElement(nums, val)
         print(res)
 
     def removeElement(self, nums: List[int], val: int) -> int:
+
         c = 0
-        for i in range(len(nums) - 1):
-            if nums[i] == val:
+        for k in nums:
+            if k == val:
                 c += 1
 
-        for i in range(len(nums) - c):
-            if nums[i] == val:
-                nums[i] = nums[i + 1]
+        if c == 0:
+            return len(nums)
+        elif c == len(nums):
+            nums.clear()
+            return len(nums)
+        else:
+            i = 0
+            j = 0
+            while i < len(nums) - 1:
+                if nums[i] == val:
+                    if nums[i + 1] == val:
+                        j += 1
+                    else:
+                        nums[i - j] = nums[i + 1]
+                        nums[i + 1] = val
+                i += 1
+
+            return len(nums) - c
+
