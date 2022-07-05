6168244b6241e62eca82ca37b7804c780b1efbd0
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-07-05 21:32:33 +0600

    optimize RemoveElement.py

--- LeetCode/Python/RemoveElement.py
+++ LeetCode/Python/RemoveElement.py
@@ -4,9 +4,9 @@ from typing import List
 class RemoveElement:
 
     def __init__(self):
-        nums = [3,2,2,3]
-        val = 3
-        res = self.removeElement(nums, val)
+        nums = [0, 1, 2, 2, 3, 0, 4, 2]
+        val = 2
+        res = self.removeElementOptimized(nums, val)
         print(res)
 
     def removeElement(self, nums: List[int], val: int) -> int:
@@ -35,3 +35,26 @@ class RemoveElement:
 
             return len(nums) - c
 
+    def removeElementOptimized(self, nums: List[int], val: int) -> int:
+
+        c = 0
+        for k in nums:
+            if k == val:
+                c += 1
+
+        if c == 0:
+            return len(nums)
+        elif c == len(nums):
+            nums.clear()
+            return len(nums)
+        else:
+            i = 0
+            n = len(nums)
+            while i < n:
+                if nums[i] != val:
+                    i += 1
+                else:
+                    nums[i] = nums[n - 1]
+                    n -= 1
+
+            return n
