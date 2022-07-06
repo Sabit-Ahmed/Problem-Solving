307e895580141c69e8f61134c2ec9ebf54188333
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-07-06 19:56:49 +0600

    optimize CheckDoubleInArray.py

--- LeetCode/Python/CheckDoubleInArray.py
+++ LeetCode/Python/CheckDoubleInArray.py
@@ -4,8 +4,8 @@ from typing import List
 class CheckDoubleInArray:
 
     def __init__(self):
-        arr = [0,-1,4,-9,-8]
-        res = self.checkIfExistTwoPass(arr)
+        arr = [0, 1, 0]
+        res = self.checkIfExistOnePass(arr)
         print(res)
 
     def checkIfExistTwoPass(self, arr: List[int]) -> bool:
@@ -26,3 +26,22 @@ class CheckDoubleInArray:
             if arr[i] in hashmap.keys():
                 return True
         return False
+
+    def checkIfExistOnePass(self, arr: List[int]) -> bool:
+        hashmap = {}
+        zero_counter = 0
+        for i in range(len(arr)):
+            if arr[i] != 0:
+                product = arr[i] * 2
+                dividend = arr[i] / 2
+                hashmap[product] = i
+                hashmap[dividend] = i + 1
+                if arr[i] in hashmap.keys():
+                    return True
+            else:
+                zero_counter += 1
+
+        if zero_counter > 1:
+            return True
+        else:
+            return False
